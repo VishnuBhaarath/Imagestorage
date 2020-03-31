c30810bc91192451608c951ab7bac4d179201ba3
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-03-31 10:36:22 +0530

    Adding images

--- app/src/main/java/com/example/imagestorage/MainActivity.java
+++ app/src/main/java/com/example/imagestorage/MainActivity.java
@@ -1,17 +1,50 @@
 package com.example.imagestorage;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-private Button chooseimage1,nextactivity1;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Button chooseimage1,nextactivity1;
+    Uri mImageUri;
+    ImageView mImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         chooseimage1=(Button) findViewById(R.id.chooseimage);
+        mImageView=(ImageView)findViewById(R.id.image) ;
+        chooseimage1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openfilechooser();
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            mImageUri = data.getData();
+            mImageView.setImageURI(mImageUri);
+
+
+        }
+    }
+    private void openfilechooser(){
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,PICK_IMAGE_REQUEST);
     }
 }
