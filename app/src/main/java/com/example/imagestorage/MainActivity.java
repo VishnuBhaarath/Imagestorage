25e06a325c49b23f0659e50699b8cc1aee2da375
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-03-31 11:07:10 +0530

    Storing images in firebase

--- app/src/main/java/com/example/imagestorage/MainActivity.java
+++ app/src/main/java/com/example/imagestorage/MainActivity.java
@@ -1,33 +1,54 @@
 package com.example.imagestorage;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class MainActivity extends AppCompatActivity {
     private static final int PICK_IMAGE_REQUEST = 1;
     private Button chooseimage1,nextactivity1;
     Uri mImageUri;
     ImageView mImageView;
+    private StorageReference mStorageRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mStorageRef = FirebaseStorage.getInstance().getReference("uploads");
         chooseimage1=(Button) findViewById(R.id.chooseimage);
+        nextactivity1=(Button)findViewById(R.id.nextactivity);
         mImageView=(ImageView)findViewById(R.id.image) ;
         chooseimage1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openfilechooser();
+
             }
         });
-
+       nextactivity1.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               uploadFile();
+           }
+       });
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -47,4 +68,31 @@ public class MainActivity extends AppCompatActivity {
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(intent,PICK_IMAGE_REQUEST);
     }
+    private void uploadFile(){
+        if(mImageUri!=null){
+            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()
+                    + "." + getFileExtension(mImageUri));
+
+            Log.e("mams",mImageUri.toString());
+            fileReference.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    Toast.makeText(MainActivity.this,"Succes",Toast.LENGTH_SHORT).show();
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this,"error",Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+    private String getFileExtension(Uri uri) {
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
 }
+
